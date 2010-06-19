245df6a6ad6c1a18cc2d41e2bebf5137a832dbce
Ben Lavender <blavender@gmail.com>
2010-06-19 12:04:25 -0500

    Make results an explicit holder object to prevent re-evaluation when the result is nil or false.  Hold errors to prevent re-evaluation if an exception is raised.

--- lib/promise.rb
+++ lib/promise.rb
@@ -13,7 +13,9 @@
 #     x + 5     # => 15
 class Promise < defined?(BasicObject) ? BasicObject : Object
 
-  instance_methods.each { |m| undef_method m unless m =~ /__|object_id/ } unless defined?(BasicObject)
+  instance_methods.each { |m| undef_method m unless m.to_s =~ /__/ }
+
+  RESULT_NOT_SET = Object.new.freeze
 
   # Returns a new promise
   # @param [Proc] block
@@ -24,21 +26,27 @@ class Promise < defined?(BasicObject) ? BasicObject : Object
     end
     @block = block
     @mutex = ::Mutex.new
+    @result = RESULT_NOT_SET
+    @error  = RESULT_NOT_SET
   end
 
   # Force the evaluation of this promise immediately
   # @return [Any]
   def force
     @mutex.synchronize do
-      unless @result
-        @result = @block.call
+      if @result == RESULT_NOT_SET && @error == RESULT_NOT_SET
+        begin
+          @result = @block.call
+        rescue Exception => e
+          @error = e
+        end
       end
-    end
-    @result
+    end if @result == RESULT_NOT_SET && @error == RESULT_NOT_SET
+    @error == RESULT_NOT_SET ? @result : raise(@error)
   end
 
   def method_missing(method, *args, &block)
-    force unless @result
+    force
     @result.send(method, *args, &block)
   end
 end
